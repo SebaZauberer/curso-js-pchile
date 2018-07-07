09102853b1ccf840b144c9ed83c177036055ef64
sebazauberer <seba.olate.h@gmail.com>
2018-07-07 13:57:41 -0400

    clase 7 parte 2 falta validar los select

--- JS_clase7/main.js
+++ JS_clase7/main.js
@@ -39,3 +39,51 @@ document.addEventListener('DOMContentLoaded', function () {
         anio.appendChild(optionAnio);
     }
 });
+
+function validacion(){
+    var formulario = document.getElementById('formulario');
+
+    // valida un campo de texto
+    if(formulario.nombre == null || formulario.nombre.value.length == 0 || /^\s+$/.test(formulario.nombre.value)){
+        alert("Completa el campo nombre con un formato valido");
+        return false;
+    }
+
+    // valida un campo de numeros
+    if(formulario.edad.value == null || formulario.edad.value.length == 0 || isNaN(formulario.edad.value)){
+        alert("Completa el campo edad con un formato valido");
+        return false;
+    }
+
+    // valida una dirección de correo
+    if(formulario.email.value == null || !(/^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i.test(formulario.email.value))){
+        alert("Completa el campo email con un formato valido");
+        return false;
+    }
+
+    // valida los radio buttons
+    var radios = formulario.querySelectorAll('input[type=radio]');
+    var select = false;
+
+    for(var i = 0; i < radios.length; i++){
+        if(radios[i].checked){
+            select = true;
+            break;
+        }
+    }
+
+    if(!select){
+        alert("Selecciona al menos una opción de genero");
+        return false;
+    }
+
+    //valida un numero de telefono
+    if(formulario.telefono.value == null || !(/^\d{9}$/.test(formulario.telefono.value))){
+        alert("Completa el campo telefono con un formato valido");
+        return false;
+    }
+
+    // si ninguna de las condiciones se cumple, entonces se retorna 'true' y se envia el formulario
+    alert("formulario enviado");
+    return true;
+}
